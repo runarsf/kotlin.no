e9749e43a971ed47b11230852c98da69d57a5992
Runar <runarsf@protonmail.com>
2018-08-12 22:33:03 +0200

    Added imgs and got json to work

--- iandex/script.js
+++ iandex/script.js
@@ -1,10 +1,18 @@
 // https://www.gun.io/blog/multi-line-strings-in-json
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
-// import { talos } from "texts.json";
-// var file = talos.mla['arkady_journal77'].content.join('\n');
+// file = talos.mla['arkady_journal77'].content.join('\n');
 
-foo = "gggggggggggggggggggggggggggggggggggggggggggggggggggggggggg";
-document.getElementById("terminput").style.visibility = "hidden";
+$(function(){
+	$.getJSON('texts.json',function(data){
+		console.log('jQuery: successfully imported json');
+		$.each(data.milton,function(i,mla){
+			$('ul').append('<li>'+mla.title+' '+mla.type+'</li>');
+		});
+	}).error(function(){
+		console.log('jQuery: json error');
+	});
+});
+
 
 function scratchpad() {
 	if(document.getElementById("scratchpad").style.visibility === "visible") {
