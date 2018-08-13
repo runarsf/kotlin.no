798e5e3c5aabfdf48598c0de6f51784ae47574eb
Runar Fredagsvik <runar.fredagsvik@kantega.no>
2018-08-13 15:05:58 +0200

    Each amd everything, huge iandex improvements

--- iandex/script.js
+++ iandex/script.js
@@ -6,8 +6,9 @@ function open(textFile) {
 			$.each(data.milton, function(i, mla) {
 				if(textFile === mla.title+'.'+mla.type) {
 					$('ul').empty();
-					scratchpad();
-					$('ul').append('<li>'+mla.title+'.'+mla.type+'</li>');
+					scratchpad(show);
+					// $('ul').append('<li>'+mla.title+'.'+mla.type+'</li>');
+					document.title = mla.title+'.'+mla.type;
 					$('ul').append('<li>'+mla.content.join('<br/>')+'</li>&nbsp')
 				}
 			});
@@ -17,24 +18,62 @@ function open(textFile) {
 	});
 }
 
-
-function scratchpad() {
-	if(document.getElementById("scratchpad").style.visibility === "visible") {
-		document.getElementById("scratchpad").style.visibility = "hidden";
-		document.getElementById("bgblur").style.visibility = "hidden";
-	}
-	else {
+show = 1;
+hide = 0;
+function scratchpad(state) {
+	if(state === show) {
+		document.getElementById("allfiles").style.visibility = "hidden";
 		document.getElementById("scratchpad").style.visibility = "visible";
+		document.getElementById("button").style.visibility = "visible";
 		document.getElementById("bgblur").style.visibility = "visible";
+	}
+	else if(state === hide) {
+		document.title = "iandex";
+		document.getElementById("scratchpad").style.visibility = "hidden";
+		document.getElementById("button").style.visibility = "hidden";
+		document.getElementById("bgblur").style.visibility = "hidden";
 	}
 }
 
 document.addEventListener("keydown", event => {
-	if(event.keyCode == 13) {
+	if(event.keyCode == 13) { // enter
 		alert("asd");
+	}
+	if(event.keyCode == 9) { // tab
+		allFiles();
+	}
+	if(event.keyCode == 27) { // esc
+		allFiles("hide");
+		scratchpad(hide);
 	}
 });
 
+function allFiles(optional) {
+	if(optional === "hide") {
+		document.getElementById("allfiles").style.visibility = "hidden";
+	}
+	else {
+		scratchpad(hide);
+		allfs = document.getElementById("allfiles").style.visibility;
+		if(allfs !== "visible") {
+			document.getElementById("allfiles").style.visibility = "visible";
+			$(function(){
+				$.getJSON('texts.json', function(data) {
+					console.log('jQuery: successfully imported json');
+					$('ul').empty();
+					$.each(data.milton, function(i, mla) {
+						$('ul').append('<li>'+mla.title+'.'+mla.type+mla.adminNotes+'</li>');
+					});
+				}).error(function(){
+					console.log('jQuery: json error');
+				});
+			});
+		}
+		else {
+			allFiles("hide");
+		}
+	}
+}
 
 function colorScheme(bgColor, textColor, rulerColor) {
 	document.getElementById("body").style.backgroundColor = bgColor;
