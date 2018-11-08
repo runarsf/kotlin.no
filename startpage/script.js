a4f4eaea7a4664dcb0a9873fad149c05abcf294f
Runar <runarsf@protonmail.com>
2018-11-08 22:31:29 +0100

    started to work on new startapage

--- startpage/script.js
+++ startpage/script.js
@@ -1,11 +1,8 @@
 // TODO: add wallpaper support
-// TODO: convert all links to linkIt()
 $(document).ready(
 	function()
 	{
-		currentEngine();
-		currentMode();
-		currentBox();
+		
 	}
 );
 
@@ -45,20 +42,12 @@ function keyRegister(keyCode1, keyCode2, runFunction) {
 }
 keyRegister(71, 71, google);		// gg
 keyRegister(84, 82, translate);		// tr
-keyRegister(16, 71, selfDashboard);	// Shift g
-keyRegister(71, 65, dashboard);		// ga
 keyRegister(82, 82, reload);		// rr
 keyRegister(112, null, helpMe);		// F1
-keyRegister(71, 76, gitLab);		// gl
 keyRegister(71, 72, gitHub);		// gh
-keyRegister(82, 87, randomWord)		// rw
 function gitHub() { linkIt('https://github.com/') };
-function gitLab() {	linkIt('https://gitlab.kantega.no/') };
-function selfDashboard() { linkIt('https://jira.kantega.no/secure/RapidBoard.jspa?rapidView=100&quickFilter=635') };
-function dashboard() { linkIt('https://jira.kantega.no/secure/RapidBoard.jspa?rapidView=100') };
 function google() { linkIt('https://google.com/') };
 function translate() { linkIt('https://translate.google.com/') };
-function randomWord() { linkIt('https://randomwordgenerator.com/') };
 function reload() { location.reload() };
 
 
@@ -75,44 +64,6 @@ function helpMe() {
 	}
 }
 
-function linkIt(url) {
-	redir = getCookie('redirect');
-	if(redir !== "true" || redir !== "false") {
-		redir = "false";
-	}
-	if(url === "") {
-		alert("Missing URL");
-	}
-	else if(redir === "true") {
-		window.open(url, "_self");
-	}
-	else if(redir === "false") {
-		window.open(url);
-	}
-}
-
-function setBox() {
-	var checkBox = document.getElementById("selfOpen");
-
-	if(checkBox.checked === true) {
-		setCookie('redirect', "true");
-	}
-	else {
-		setCookie('redirect', "false");
-	}
-}
-function currentBox() {
-	dirc = getCookie('redirect');
-	if(dirc === "true") {
-		document.getElementById('selfOpen').checked = true;
-		console.log("check");
-	} else {
-		document.getElementById('selfOpen').checked = false;
-		console.log("uncheck");
-	}
-}
-
-
 function startTime() {
 	var today = new Date();
 	var h = today.getHours();
@@ -128,65 +79,7 @@ function checkTime(i) {
 	return i;
 }
 
-
-function importFile(file) {
-	if(file.endsWith('css')) {
-		$('head').append('<link rel="stylesheet" type="text/css" media="screen" href="'+file+'" />');
-	}
-	if(file.endsWith('js')) {
-		$('head').append('<script type="text/javascript" src="'+file+'"></script>');
-	}
-}
-
-
-function favesJson() {
-	$(function(){
-		$.getJSON('content.json', function(data) {
-			console.log('jQuery: successfully imported json');
-			$.each(data, function(i, content) {
-				// parse = content.join().replace(/https:\/\//g , '<a href=\'https://').replace(/,/g , '\'>'+'asd'+'</a><br/>')
-				parse = content.join('<br />');
-				$('#faves ul').append('<li>'+parse+'</li>');
-			});
-		}).error(function(){
-			console.log('jQuery: json error');
-		});
-	});
-}
-
-function favAdd() {
-	title = document.getElementById('inpTITLE').value;
-	url = document.getElementById('inpURL').value;
-	if(title === '' || url === '') {
-		alert('Title or URL cannot be blank');
-	}
-	if(url.startsWith('http://') || url.startsWith('https://') && url.includes('.')) {
-		$('#faves ul').append('<li><a onclick=\'linkIt("'+url+'")\'>'+title+'</a></li>');
-		document.getElementById('inpTITLE').value = '';
-		document.getElementById('inpURL').value = '';
-		xi = document.querySelectorAll('#faves ul li').length;
-		if(xi >= 1) {
-			document.getElementById('faves').style.display = 'initial';
-		}
-		else {
-			document.getElementById('faves').style.display = 'none';
-		}
-	}
-	else {
-		alert('URL has to start with http(s)://')
-	}
-}
-
-function favBox() {
-	box = document.getElementById('inputbox');
-	if(box.style.visibility === 'visible') {
-		box.style.visibility = 'hidden';
-	} else {
-		box.style.visibility = 'visible';
-	}
-}
-
-var tld = ["com", "net", "org", "no", "hiphop", "coffee"];
+var tld = ["com", "net", "org", "no", "hiphop", "coffee", "io"];
 function search(e) {
 	currentEngine();
 	if (e.keyCode == 13) { // enter
@@ -275,56 +168,6 @@ function getCookie(cname) {
 		}
 	}
 	return '';
-}
-
-
-
-
-
-
-
-function colorScheme(bgColor, textColor, rulerColor, searchColor) {
-	document.getElementById('body').style.backgroundColor = bgColor;
-	document.getElementById('html').style.backgroundColor = bgColor;
-	document.getElementById('body').style.color = textColor;
-	document.getElementById('html').style.color = textColor;
-
-	document.getElementById('info').style.backgroundColor = bgColor;
-	document.getElementById('binds').style.color = textColor;
-	document.getElementById('infoTitle').style.color = textColor;
-	document.getElementById('infoH1').style.color = textColor;
-	document.getElementById('infoH2').style.color = textColor;
-	document.getElementById('infoHR1').style.backgroundColor = rulerColor;
-	document.getElementById('infoHR2').style.backgroundColor = rulerColor;
-	document.getElementById('infoHR3').style.backgroundColor = rulerColor;
-	document.getElementById('search-field').style.backgroundColor = searchColor;
-	document.getElementById('search-field').style.color = bgColor;
-	document.getElementById('search-text').style.color = searchColor;
-}
-function darkMode() {
-	setCookie('colorMode', 'dark');
-	colorScheme('#222', '#FFF', '#5a5a5a', '#dedede');
-}
-function lightMode() {
-	setCookie('colorMode', 'light');
-	colorScheme('#FFF', '#222', '#bebebe', '#272727');
-}
-function toggleMode() {
-	var cm = getCookie('colorMode');
-	if (cm === 'light') {
-		darkMode();
-	} else {
-		lightMode();
-	}
-}
-function currentMode() {
-	var cm = getCookie('colorMode');
-	if (cm === 'light') {
-		lightMode();
-	} else {
-		darkMode();
-	}
-	return cm;
 }
 
 let ngin;
