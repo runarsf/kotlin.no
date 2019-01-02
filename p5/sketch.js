de27c570b924f3a93b3d71f6496cf810885a08a0
Runar <runarsf@protonmail.com>
2019-01-02 23:21:30 +0100

    correct dimension, won't be using p5

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,28 +1,36 @@
-var borderWidth = 50;
+var borderSize = 100;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     background(0);
 
-    fill(0,0,0,0);
+    // outter main border
+    fill(0);
     stroke(255,220,215);
-    strokeWeight(borderWidth);
-    rect(0,0,window.innerWidth, window.innerHeight);
-    strokeWeight(2);
-    rect(borderWidth/2+5,borderWidth/2+5,window.innerWidth-borderWidth-10, window.innerHeight-borderWidth-35);
-    noStroke();
-    fill(255,220,215);
-    rect(borderWidth/2, window.innerHeight-borderWidth, window.innerWidth-borderWidth, window.innerHeight-borderWidth);
+    strokeWeight(borderSize);
+    rect(0-borderSize/4, 0-borderSize/4, width+borderSize/2, height+borderSize/8);
 
-    fill(255,255,255);
+    // inner thin border
+    strokeWeight(2);
+    rect(0+borderSize/3, 0+borderSize/3, width-borderSize/1.5, height-borderSize-borderSize/15);
+
+    // draw old-monitor-like lines
+    fill(255);
     stroke(50);
     strokeWeight(1);
-    for(lineX = 0; lineX <= window.innerHeight; lineX = lineX + 2) {
-        line(0, lineX, window.innerWidth, lineX);
+    for(lineX = 0; lineX <= height; lineX = lineX + 2) {
+        line(0, lineX, width, lineX);
     }
-    fill(0,0,0);
 }
 
+
+
 function draw() {
-    text("hello", 100, 100);
+    fill(255);
+    stroke(0);
+    text(1, 100, 100);
+}
+
+function windowResized() {
+    setup();
 }
