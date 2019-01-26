95b77b8b28b10ee83022160659141e298ed812fc
Runar <runarsf@protonmail.com>
2019-01-26 11:17:43 +0100

    general update

--- startpage/neon/script.js
+++ startpage/neon/script.js
@@ -1,8 +1,27 @@
+// window.getComputedStyle(document.documentElement).getPropertyValue('--wallcolor')
+
+$(document).keypress(function(e) {
+    stdin = document.getElementById('stdin');
+    stdin.focus();
+});
+function tput(ln) {
+    if(ln) { $('#stdout').append(ln+'<br/>'); }
+    else { $('#stdout').append(window.getComputedStyle(document.documentElement).getPropertyValue('--prefix').replace(/\'+/g, '')+'<br/>'); }
+}
+
 function parse(e) {
     stdin = document.getElementById('stdin');
+    stdout = document.getElementById('stdout');
 
-    if(e.keyCode === 13) {
-        console.log(e);
-        stdin.value = '';
+    switch(e.keyCode) {
+        case 13:
+            tput(stdin.value);
+            stdin.value = '';
+            break;
     }
+}
+
+function updateScroll(){
+    var element = document.getElementById('stdout');
+    element.scrollTop = element.scrollHeight;
 }
