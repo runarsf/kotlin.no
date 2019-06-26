2d518ff053c3ad0a061ef846f0d5171c8ac4122e
Runar <runarsf@protonmail.com>
2019-06-26 21:55:51 +0200

    added math and made positioning more dynamic, minor resizing improvements

--- startpage/cobalt/script.js
+++ startpage/cobalt/script.js
@@ -78,13 +78,8 @@ jQuery.expr[':'].contains = function (a, i, m) {
     return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
 };
 
-/**
- * Search links
- * Triggered on key UP -> Important because key binds are run on key DOWN, and the binds need to execute first.
- */
-
 var trigger = {
-    search: function () {
+    search: function () { // keyup
         var text = $('#search-input').val();
 
         // Hide all content class element (.css added for more flexibility)
@@ -99,20 +94,21 @@ var trigger = {
             return $(this).find('li:visible').length == 0;
         }).hide();
 
+        document.getElementById('quick-result').innerHTML = this.maths(text);
         
-        var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        /*var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
         // location.href = "mailto:"+emailTo+'?cc='+emailCC+'&subject='+emailSub+'&body='+emailBody;
         if (re.test(String(text).toLowerCase())) {
             $('#mailbox').show();
         } else {
             $('#mailbox').hide();
-        }
+        }*/
     },
-    binds: function (event) {
+    binds: function (event) { // keydown
         if (event.defaultPrevented) return;
 
         let input = document.getElementById('search-input');
-        /*input.focus();*/
+        if (!($("input").is(":focus")) && !($("textarea").is(":focus")) && !($("#search-prefix").is(":focus"))) input.focus();
 
         let key = event.key || event.keyCode || event.which;
 
@@ -123,6 +119,23 @@ var trigger = {
                 input.value = '';
                 input.blur();
             }
+        } else if (!($("input").is(":focus")) && key === 'Return' || key === 'Enter' || key === 13) {
+            this.engine(text);
         }
+    },
+    maths: function (query) {
+        let returns = '';
+        let evaluation = '';
+        
+        if (query.toLowerCase() === 'help') returns = 'i = 1; i++; i';
+        if (query.toLowerCase() === 'e') returns = Math.E;
+        if (query.toLowerCase() === 'pi') returns = Math.PI;
+        
+        try { evaluation = eval(query); } catch (e) { evaluation = ''; }
+        if (typeof evaluation !== 'undefined' && evaluation) returns = evaluation;
+        return returns;
+    },
+    engine: function (query) {
+        
     }
 }
